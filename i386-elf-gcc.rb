343b74183512ff0b7878dfec2923fd45eea86f84
Gautham Badhrinathan <b.gautham@gmail.com>
2014-04-10 03:41:12 -0700

    Update i386-elf-gcc.rb

--- i386-elf-gcc.rb
+++ i386-elf-gcc.rb
@@ -4,7 +4,7 @@ class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftpmirror.gnu.org/gcc/gcc-4.6.1/gcc-4.6.1.tar.bz2'
   mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.6.1/gcc-4.6.1.tar.bz2'
-  sha1 'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
+  sha1 '8bf66f7a71534ba564563a0ba0b2517aead8ac30'
 
   depends_on 'gmp'
   depends_on 'libmpc'
