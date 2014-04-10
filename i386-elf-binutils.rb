cdb205a96cf6f337c066805abe24aeb5fb8c93ad
Gautham Badhrinathan <b.gautham@gmail.com>
2014-04-10 04:09:49 -0700

    Update i386-elf-binutils.rb

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -2,8 +2,7 @@ require 'formula'
 
 class I386ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
-  sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.14.tar.gz'
 
   depends_on 'apple-gcc42' => :build
 
